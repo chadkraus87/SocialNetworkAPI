3873aa65aebad8f24a62c23a84589607c1d51e3d
chadkraus87 <127354484+chadkraus87@users.noreply.github.com>
2023-07-25 17:44:27 -0500

    Fixed server.js

--- routes/api/reactionRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createReaction, deleteReaction } = require('../controllers/api/reactionController');
-
-router.post('/thoughts/:thoughtId/reactions', createReaction);
-router.delete('/thoughts/:thoughtId/reactions/:reactionId', deleteReaction);
-
-module.exports = router;
--- server.js
+++ server.js
@@ -11,12 +11,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// Error handling
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).json({ error: 'Server error' });
-});
-
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server for running on port ${PORT}!`);
