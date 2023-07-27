a2ee18e648c66622dd32d47328599be116fe4dc9
chadkraus87 <127354484+chadkraus87@users.noreply.github.com>
2023-07-27 12:39:34 -0500

    Fixed delete Thought route.

--- controllers/api/thoughtController.js
+++ controllers/api/thoughtController.js
@@ -1,62 +1,66 @@
-const Thought = require('../../models/Thought');
-const User = require('../../models/User');
+const Thought = require("../../models/Thought");
+const User = require("../../models/User");
+const Reaction = require("../../models/Reaction");
 
 const createThought = async (req, res) => {
   try {
     const { thoughtText, username } = req.body;
-    const thought = await Thought.create({ thoughtText, username });
 
-    const user = await User.findById(req.body.userId);
+    const user = await User.findOne({ username });
+
     if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({ message: "User not found" });
     }
+
+    const thought = await Thought.create({ thoughtText, username });
     user.thoughts.push(thought._id);
     await user.save();
 
     res.status(201).json(thought);
   } catch (error) {
-    console.error('Error creating thought:', error);
-    res.status(500).json({ error: 'Failed to create thought' });
+    console.error("Error creating thought:", error);
+    res.status(500).json({ error: "Failed to create thought" });
   }
 };
 
 const getThoughtById = async (req, res) => {
   try {
-    const thought = await Thought.findById(req.params.thoughtId).populate('reactions');
+    const thought = await Thought.findById(req.params.thoughtId).populate(
+      "reactions"
+    );
     if (!thought) {
-      return res.status(404).json({ message: 'Thought not found' });
+      return res.status(404).json({ message: "Thought not found" });
     }
     res.json(thought);
   } catch (error) {
-    console.error('Error getting thought by ID:', error);
-    res.status(500).json({ error: 'Failed to get thought' });
+    console.error("Error getting thought by ID:", error);
+    res.status(500).json({ error: "Failed to get thought" });
   }
 };
 
 const updateThought = async (req, res) => {
   try {
-    const thought = await Thought.findByIdAndUpdate(req.params.thoughtId, req.body, { new: true });
+    const thought = await Thought.findByIdAndUpdate(
+      req.params.thoughtId,
+      req.body,
+      { new: true }
+    );
     if (!thought) {
-      return res.status(404).json({ message: 'Thought not found' });
+      return res.status(404).json({ message: "Thought not found" });
     }
     res.json(thought);
   } catch (error) {
-    console.error('Error updating thought:', error);
-    res.status(500).json({ error: 'Failed to update thought' });
+    console.error("Error updating thought:", error);
+    res.status(500).json({ error: "Failed to update thought" });
   }
 };
 
 const deleteThought = async (req, res) => {
   try {
-    const thought = await Thought.findByIdAndDelete(req.params.thoughtId);
-    if (!thought) {
-      return res.status(404).json({ message: 'Thought not found' });
+    const deletedThought = await Thought.findByIdAndDelete(req.params.thoughtId);
+    if (!deletedThought) {
+      return res.status(404).json({ error: 'Thought not found' });
     }
-
-    const user = await User.findById(thought.userId);
-    user.thoughts = user.thoughts.filter((thoughtId) => thoughtId.toString() !== req.params.thoughtId);
-    await user.save();
-
     res.json({ message: 'Thought deleted successfully' });
   } catch (error) {
     console.error('Error deleting thought:', error);
@@ -69,8 +73,8 @@ const getAllThoughts = async (req, res) => {
     const thoughts = await Thought.find();
     res.json(thoughts);
   } catch (error) {
-    console.error('Error getting all thoughts:', error);
-    res.status(500).json({ error: 'Failed to get thoughts' });
+    console.error("Error getting all thoughts:", error);
+    res.status(500).json({ error: "Failed to get thoughts" });
   }
 };
 
